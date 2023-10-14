95bdc5e343614d6673b9944ddcc7886ebfd5d960
PreciousSands <precious.sands15@gmail.com>
2023-10-13 23:54:34 -0300

    First grid created

--- second_app.js
+++ second_app.js
@@ -7,28 +7,43 @@
 /*----- constants -----*/
 const cookieContainer = document.querySelector(`.cookie-container`);
 const turnButton = document.getElementById(`turnButton`);
+const gamesBoardContainer = document.querySelector(`#gamesboard-container`)
 const cookies = Array.from(cookieContainer.children)
+
+
 
 /*----- state variables -----*/
 let angle = 0
+const width = 10
 
 /*----- cached elements  -----*/
 
 
 /*----- event listeners -----*/
-turnButton.addEventListener(`click`,turn);
+turnButton.addEventListener(`click`, turn);
 
 
 
 
 
 /*----- functions -----*/
-function turn () { 
-console.log("Rotation is sure enough in motion" , cookieContainer.children)
-if (angle === 0) {
-    angle = 90; 
-} else {
-    angle = 0;
+function turn() {
+    if (angle === 0) {
+        angle = 90;
+    } else {
+        angle = 0;
+    }
+    cookies.forEach(cookie => cookie.style.transform = `rotate(${angle}deg)`)
+    console.log("Rotation is sure enough in motion", cookieContainer.children)
 }
-cookies.forEach(cookies => cookies.style.transform = `rotate(${angle}deg)`)
+
+
+function createGrid() {
+    for (let i = 0; i < width; i++) {
+        const gameBoardContainer = document.createElement(`div`);
+        gameBoardContainer.classList.add(`game-board`);
+        gamesBoardContainer.appendChild(gameBoardContainer);
+    }
 }
+
+createGrid();
