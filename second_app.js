c94f2e93a7f02b9222e312ca9e42adec202e0c1f
PreciousSands <precious.sands15@gmail.com>
2023-10-16 21:59:45 -0300

    User can see player and computer grid layout

--- second_app.js
+++ second_app.js
@@ -9,12 +9,12 @@ const cookieContainer = document.querySelector(`.cookie-container`);
 const turnButton = document.getElementById(`turnButton`);
 const gamesBoardContainer = document.querySelector(`#gamesboard-container`)
 const cookies = Array.from(cookieContainer.children)
-
+const width = 10
 
 
 /*----- state variables -----*/
 let angle = 0
-const width = 10
+
 
 /*----- cached elements  -----*/
 
@@ -37,13 +37,20 @@ function turn() {
     console.log("Rotation is sure enough in motion", cookieContainer.children)
 }
 
+function createGrid(color, user) {
+    const gameBoardContainer = document.createElement(`div`);
+    gameBoardContainer.classList.add(`game-board`);
+    gameBoardContainer.style.backgroundColor = color
+    gameBoardContainer.id = user
+    
 
-function createGrid() {
-    for (let i = 0; i < width; i++) {
-        const gameBoardContainer = document.createElement(`div`);
-        gameBoardContainer.classList.add(`game-board`);
-        gamesBoardContainer.appendChild(gameBoardContainer);
+    for(let i = 0; i < width * width; i++) {
+        const circle = document.createElement(`div`)
+        circle.classList.add(`circle`)
+        circle.id = i
+        gameBoardContainer.append(circle)
     }
+    gamesBoardContainer.append(gameBoardContainer);
 }
-
-createGrid();
+createGrid(`gold`, `player`)
+createGrid(`blue`, `computer`)
