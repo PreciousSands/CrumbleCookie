4ced12adf0be37ced23dc8e2addc4de0d20fd681
PreciousSands <precious.sands15@gmail.com>
2023-10-19 00:49:07 -0300

    User can see computer's cookies. They're blocks for now as I am running up on time to get my game working, I'll try adding pictures instead of blocks later

--- second_app.js
+++ second_app.js
@@ -5,52 +5,184 @@
 // Creation of win and lose
 
 /*----- constants -----*/
-const cookieContainer = document.querySelector(`.cookie-container`);
-const turnButton = document.getElementById(`turnButton`);
-const gamesBoardContainer = document.querySelector(`#gamesboard-container`)
-const cookies = Array.from(cookieContainer.children)
+const cookieContainer = document.querySelector('.cookie-container');
+const turnButton = document.getElementById('turn-button');
+const gamesBoardContainer = document.querySelector('#gamesboard-container')
+const optionCookies = Array.from(cookieContainer.children)
 const width = 10
+const allPlayerCookieBlocks = document.querySelectorAll(`#player div`)
 
 
-/*----- state variables -----*/
+
+// /*----- state variables -----*/
 let angle = 0
+let draggedCookie;
+// /*----- cached elements  -----*/
 
 
-/*----- cached elements  -----*/
+// /*----- event listeners -----*/
+turnButton.addEventListener('click', turn);
+optionCookies.forEach(optionCookie => optionCookie.addEventListener('dragstart', dragStart))
+allPlayerCookieBlocks.forEach(playerCookie => {
+    playerCookie.addEventListener(`dragover`, dragOver)
+    playerCookie.addEventListener(`drop`, dropCookie)
+})
 
 
-/*----- event listeners -----*/
-turnButton.addEventListener(`click`, turn);
 
-
-
-
-
-/*----- functions -----*/
+// /*----- functions -----*/
 function turn() {
+    const optionCookies = Array.from(cookieContainer.children)
     if (angle === 0) {
         angle = 90;
     } else {
         angle = 0;
     }
-    cookies.forEach(cookie => cookie.style.transform = `rotate(${angle}deg)`)
+    optionCookies.forEach(optionCookie => optionCookie.style.transform = `rotate(${angle}deg)`)
     console.log("Rotation is sure enough in motion", cookieContainer.children)
 }
 
 function createGrid(color, user) {
-    const gameBoardContainer = document.createElement(`div`);
-    gameBoardContainer.classList.add(`game-board`);
+    const gameBoardContainer = document.createElement('div');
+    gameBoardContainer.classList.add('game-board');
     gameBoardContainer.style.backgroundColor = color
     gameBoardContainer.id = user
-    
 
-    for(let i = 0; i < width * width; i++) {
-        const circle = document.createElement(`div`)
-        circle.classList.add(`circle`)
-        circle.id = i
-        gameBoardContainer.append(circle)
+
+    for (let i = 0; i < width * width; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.id = i
+        gameBoardContainer.append(block)
     }
     gamesBoardContainer.append(gameBoardContainer);
 }
-createGrid(`gold`, `player`)
-createGrid(`blue`, `computer`)
+createGrid('blue', 'player')
+createGrid('yellow', 'computer')
+
+
+class Cookie {
+    constructor(name, length) {
+        this.name = name;
+        this.length = length;
+    }
+}
+const cruncher = new Cookie('cruncher', 2)
+console.log(cruncher)
+const macODame = new Cookie('mac-o-dame', 3)
+console.log(macODame)
+const swirler = new Cookie('swirler', 3)
+console.log(swirler)
+const nuttyWhip = new Cookie('nutty-whip', 4)
+console.log(nuttyWhip)
+const crumbler = new Cookie('crumbler', 5)
+console.log(crumbler)
+
+
+const cookies = [cruncher, macODame, swirler, nuttyWhip, crumbler]
+
+let notDropped;
+
+
+function addCookie(user, cookie, startId) {
+    const allCookieBlocks = document.querySelectorAll(`#${user} div`);
+    console.log(allCookieBlocks);
+    let randomBoolean = Math.random() < 0.5;
+    let isHorizontal;
+    if (user === 'player') {
+        if (angle === 0) {
+            isHorizontal = true;
+        } else {
+            isHorizontal = randomBoolean;
+        }
+    }
+    let randomStartIndex = Math.floor(Math.random() * width * width);
+    console.log(randomStartIndex);
+
+    let startIndex;
+
+    if (startId) {
+        startIndex = startId;
+    } else {
+        startIndex = randomStartIndex;
+    }
+
+    let validStart;
+
+    if (isHorizontal) {
+        if (startIndex <= width * width - cookie.length) {
+            validStart = startIndex;
+        } else {
+            validStart = width * width - cookie.length;
+        }
+    } else {
+        if (startIndex <= width * width - width * cookie.length) {
+            validStart = startIndex;
+        } else {
+            validStart = randomStartIndex - (cookie.length * width) + width;
+        }
+        console.log(validStart)
+    }
+
+
+    let cookieBlocks = [];
+
+    for (let i = 0; i < cookie.length; i++) {
+        if (isHorizontal) {
+            console.log(allCookieBlocks[Number(validStart) + i]);
+            cookieBlocks.push(allCookieBlocks[Number(validStart) + i]);
+        } else {
+            cookieBlocks.push(allCookieBlocks[Number(validStart) + i * width]);
+
+        }
+    }
+
+    let valid;
+
+    if (isHorizontal) {
+        cookieBlocks.every((_cookieBlock, index) =>
+            valid = cookieBlocks[0].id % width !== width - (cookieBlocks.length - (index + 1)))
+    } else {
+        cookieBlocks.every((_cookieBlock, index) =>
+            valid = cookieBlocks[0].id < 90 + (width * index + 1)
+        )
+    }
+
+    const notOccupied = cookieBlocks.every(cookieBlock => !cookieBlock.classList.contains('occupied'))
+
+    if (valid && notOccupied) {
+        console.log(cookieBlocks)
+        cookieBlocks.forEach(cookieBlock => {
+            cookieBlock.classList.add(cookie.name)
+            cookieBlock.classList.add('occupied')
+        })
+    } else {
+        if (user === 'computer') addCookie(cookie)
+        if (user === 'player') notDropped = true
+    }
+
+}
+
+
+cookies.forEach(cookie => addCookie('computer', cookie))
+
+
+
+function dragStart(e) {
+    console.log(e.target);
+    notDropped = false;
+    draggedCookie = e.target;
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dropCookie(e) {
+    const startId = e.target.id;
+    const cookie = cookies[draggedCookie.id];
+    addCookie('player', cookie, startId);
+    if (!notDropped) {
+        draggedCookie.remove();
+    }
+}
